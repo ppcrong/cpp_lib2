2ebe26157a9daa93d0ceea8b32ae683d928feb91
yr.deng <yr.deng@atspace.com.au>
2022-09-02 14:37:21 +0800

    add class FuncMinus

---
+++ FuncMinus.cpp
@@ -0,0 +1,14 @@
+#include "FuncMinus.h"
+
+FuncMinus::FuncMinus()
+{
+}
+
+FuncMinus::~FuncMinus()
+{
+}
+
+int32_t FuncMinus::minus(int32_t a, int32_t b)
+{
+    return a - b;
+}
---
+++ include/FuncMinus.h
@@ -0,0 +1,18 @@
+#ifndef FUNCMINUS_H
+#define FUNCMINUS_H
+
+#pragma once
+
+#include <stdint.h>
+
+class FuncMinus
+{
+public:
+    FuncMinus();
+    ~FuncMinus();
+    int32_t minus(int32_t a, int32_t b);
+
+private:
+};
+
+#endif
